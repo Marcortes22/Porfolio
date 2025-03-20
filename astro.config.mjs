c6ca64f36ca2406ebcdc947d220062888ae420fd
Marco Cortes <marcortes.stiven@gmail.com>
2025-03-20 16:12:32 -0600

    add projects view

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import analogjsangular from '@analogjs/astro-angular';
-
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
